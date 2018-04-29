1d650245d89e2435b165ad50ded1f669a3b09c2d
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-29 17:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,9 @@ function distanceTravelledInFeet(from, to) {
 
 function calculatesFarePrice(from, to) {
   const chargedFeet = distanceTravelledInFeet(from, to) - 400
-  
+  const price
+  if (chargedFeet < 2000) {
+    price = chargedFeet * 0.02
+  } 
+  return price
 }
