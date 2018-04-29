ccd0426fad8b62c9f9935afad61c8d16ea66e143
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-29 16:57:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+  if (street >= 42) {
+    return street - 42
+  } else {
+    return 42 - street
+  }
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264
+}
+
+function distanceTravelledInFeet(from, to) {
+  const distance = from >= to ? from - to : to - from
+  return distance * 264
+}
+
+function calculatesFarePrice(start, destination) {
+
+}
