cb13fb02d4bb248389ee8d0c9ce59ce6b4b3aa3e
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-29 17:15:35 +0000

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ function distanceTravelledInFeet(from, to) {
   return distance * 264
 }
 
-function calculatesFarePrice(start, destination) {
-
+function calculatesFarePrice(from, to) {
+  const chargedFeet = distanceTravelledInFeet(from, to)
+  let price
+  if (chargedFeet <= 400 ) {
+    price = 0
+  } else if (chargedFeet < 2000) {
+    price = (chargedFeet - 400) * 0.02
+  } else if (chargedFeet > 2000 && chargedFeet < 2500) {
+    price = 25
+  } else {
+    price = 'cannot travel that far'
+  }
+  return price
 }
