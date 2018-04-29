fecc7f56dce70b00bc8e6cc4a86e5132651f14b1
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-29 17:14:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function calculatesFarePrice(from, to) {
   if (chargedFeet <= 400 ) {
     price = 0
   } else if (chargedFeet < 2000) {
-    price = chargedFeet * 0.02
+    price = chargedFeet * 0.02 - 400
   } else if (chargedFeet > 2000 && chargedFeet < 2500) {
     price = 25
   } else {
